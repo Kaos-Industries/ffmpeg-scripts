e0d30ce8b071e0d723742362010b8351bb3c94be
Hashim <hashaziz@hotmail.com>
2022-05-05 02:56:33 +0100

    Updates to video-from-image scirpt: add advanced options and other improvements

--- ffmpeg-video-from-image-and-crossfade-outro.sh
+++ ffmpeg-video-from-image-and-crossfade-outro.sh
@@ -1,49 +1,77 @@
 #!/bin/bash
 set -e
-if [ $# -lt 2 ]; then
+usage() {
+	echo
 	echo "Pass an image input and an audio input."
-	echo "Usage: `basename $0` image.jpg audio.flac"
+	echo "usage: `basename $0` frame.jpg audio.opus"
+	echo " -h --help     Print this help."
+	echo " -f --final    Disable the ultrafast preset to produce a final file."
 	exit
+}
+if [ $# -lt 2 ]; then usage
 else
-	read -p "Enter target resolution [default: 1920x1080]: " res
-	if ! [[ "$res" =~ ^[0-9]+x[0-9]+$ ]]; then res=1920x1080 &&
-		echo "Defaulting to 1920x1080."
-	fi
+	preset="-preset ultrafast"
+	options=$(getopt -l "final,help" -o "fh" -a -- "$@")
+	eval set -- "$options"
+	while true
+	do
+		case $1 in
+		-f|--final) 
+		    preset=""
+		    ;;
+		-h|--help) 
+		    usage
+			shift
+		    ;;
+		--)
+		    shift
+		    break;;
+		\?) 
+			echo "$OPTARG is not a valid option."
+			usage
+			shift
+			break;;    
+		esac
+		shift
+	done
 	read -p "Enter fade duration in seconds: " -i 2 -e fadeduration
 	if [[ -z "$fadeduration" || "$fadeduration" -eq 2 ]]; then 
 		fadeduration=2 
 		echo "Defaulting to 2 seconds."
 	fi
-	read -p "Start fade at custom time in first input(s)? [y/N] " -n1 -r
+	read -p "Start fade at custom time in first input? [y/N] " -n1 -r
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		while [[ -z "$fadetime" ]]; do
 		echo
 		read -p "Enter custom start time in seconds: " fadetime
+		echo "WARNING: using custom fade time of $fadetime seconds from first input.".
 	done
   fi
   img="$1"			# process the first argument and then remove it from the  
 	shift					# arguments array before looping through the rest of it  
 	for i in "$@"; do
-	  length1="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$i")"
-	  length2="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 outro.mp4)"
+		length1="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$1" | tr -d $'\r')"
+		length2="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 outro.mp4 | tr -d $'\r')"
 		if [[ -z "$fadetime" ]]; then fadetime="$(echo "$length1" - "$fadeduration" | tr -d $'\r' | bc)" && 
 			echo "Defaulting to adding fade near the end of the first input."
 		fi
 	 	total=$(echo "$length1 + $length2 - $fadeduration" | tr -d $'\r' | bc)
 		ffmpeg -y -loop 1 -t 2 -i "$img" -i "$i" -i "outro.mp4" \
+		-preset ultrafast \
 		-movflags faststart \
 		-filter_complex \
-		"color=black:$res:d=$total[base]; \
-		[0:v]setpts=PTS-STARTPTS[v0]; \
-		[2:v]format=yuva420p,fade=in:st=0:d=$fadeduration:alpha=1,setpts=PTS-STARTPTS+(($fadetime)/TB)[v1]; \
-		[base][v0]overlay[tmp]; \
-		[tmp][v1]overlay,format=yuv420p[fv]; \
+		"color=black:1920x1080:d=$total[base];
+		[0:v]fade=in:st=0:d=2,scale=-2:'max(1080,ih)',setpts=PTS-STARTPTS[v0];
+		[2:v]fade=in:st=0:d=$fadeduration:alpha=1,setpts=PTS-STARTPTS+(($fadetime)/TB)[v2];
+		[base][v0]overlay[tmp];
+		[tmp][v2]overlay,format=yuv420p[fv];
 		[1:a]afade=out:st=$fadetime:d=$fadeduration[1a];
-		[1a][2:a]concat=n=2:v=0:a=1[outa]" \
+		[1a][2:a]concat=n=2:v=0:a=1[fa]" \
 		-map [fv] -map [fa] -map -0:v:1 -map_metadata -1 -c:v libx264 -c:a libopus "${i%.*}.mp4"
 		unset fadetime
 	done
 fi
 
-# fade=in:st=0:d=2,scale=$res (0:v)
+# atrim=35.5:3620
+# -t 2
 # acrossfade=d=$fadeduration
