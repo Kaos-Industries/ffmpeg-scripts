6be18a2daf19250313fba8fcfb21531f225da9a8
Hashim <hashaziz@hotmail.com>
2022-11-29 02:28:53 +0000

    Fix globbing issue by enabling nullglob; use user-provided timestamp and remove redundant conditional; add comments

--- ffmpeg-generate-thumbnail.sh
+++ ffmpeg-generate-thumbnail.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
-shopt -s globstar
-shopt -s nocaseglob
-for video in **/*.{webm,mp4,mkv}; do
-  if [ -z "$video" ]; then
-  echo "No matching videos found in the current directory." && 
-  exit
-  fi
-  read -rp "Generate thumbnail from $video? [N/y] " -n 1
+shopt -s globstar # enable recursive globbing
+shopt -s nocaseglob # make glob case-insensitive
+shopt -s nullglob # make glob fail on non-matches instead of printing literal pattern
+for video in **/*.{webm,mp4,mkv}; do # looping through files is easier than tab-completing right-to-left filenames
+  echo
+  read -rp "Generate thumbnail from $video? [N/y]" -n 1
   if [[ $REPLY = [Yy] ]]; then
     echo
-    read -rp "What timestamp should the thumbnail be generated from?"$'\n'
-    ffmpeg -hide_banner -ss "$1" -i "$video" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 "thumb.png" 
+    read -rp "What timestamp should the thumbnail be generated from?"$'\n' timestamp
+    ffmpeg -hide_banner -ss $timestamp -i "$video" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 "thumb.png" 
   exit
   fi
 done
+echo 
+echo "No remaining videos found in the current directory."
