0b5380b5925a9a42f93528426fc2a152344d6381
Hashim <hashaziz@hotmail.com>
2022-11-29 00:15:55 +0000

    Improve generate-thumbnail

--- ffmpeg-generate-thumbnail.sh
+++ ffmpeg-generate-thumbnail.sh
@@ -3,10 +3,23 @@ set -o errexit
 set -o pipefail
 
 usage() {
-  echo "Pass a source and a timestamp to grab a thumbnail from."
-  echo "Usage: `basename $0` source.mp4 1:20"
+  echo "Pass a source to grab a thumbnail from."
+  echo "Usage: `basename $0` source.mp4"
   exit
 }
-if [ $# -lt 2 ]; then usage
-fi
-ffmpeg -ss "$2" -i "$1" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 "thumb.png" # -frames:v 10 -vsync vfr %02d.png
+if [ $# -lt 1 ]; then usage; fi
+shopt -s globstar
+shopt -s nocaseglob
+for video in **/*.{webm,mp4,mkv}; do
+  if [ -z "$video" ]; then
+  echo "No matching videos found in the current directory." && 
+  exit
+  fi
+  read -rp "Generate thumbnail from $video? [N/y] " -n 1
+  if [[ $REPLY = [Yy] ]]; then
+    echo
+    read -rp "What timestamp should the thumbnail be generated from?"$'\n'
+    ffmpeg -hide_banner -ss "$1" -i "$video" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 "thumb.png" 
+  exit
+  fi
+done
