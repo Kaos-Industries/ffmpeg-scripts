21ff0634e18cc2fc86dd687be0c402a550f01292
Hashim <hashaziz@hotmail.com>
2022-12-09 02:17:23 +0000

    Add -y to automatically overwrite; add -pred mixed to losslessly compress PNG for YouTube thumbnail limit

--- ffmpeg-generate-thumbnail.sh
+++ ffmpeg-generate-thumbnail.sh
@@ -4,13 +4,14 @@ set -o pipefail
 shopt -s globstar # enable recursive globbing
 shopt -s nocaseglob # make glob case-insensitive
 shopt -s nullglob # make glob fail on non-matches instead of printing literal pattern
-for video in **/*.{webm,mp4,mkv}; do # looping through files is easier than tab-completing right-to-left filenames
+# looping through all videos in the dir removes the pain of tab-completing right-to-left filenames
+for video in **/*.{webm,mp4,mkv}; do
   echo
   read -rp "Generate thumbnail from $video? [N/y]" -n 1
   if [[ $REPLY = [Yy] ]]; then
     echo
     read -rp "What timestamp should the thumbnail be generated from?"$'\n' timestamp
-    ffmpeg -hide_banner -ss $timestamp -i "$video" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 "thumb.png" 
+    ffmpeg -hide_banner -y -ss $timestamp -i "$video" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -frames:v 1 -pred mixed "thumb.png" 
   exit
   fi
 done
