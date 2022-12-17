0c5823668b7ef65c028507addad19c605e3afcb3
Hashim <hashaziz@hotmail.com>
2022-12-17 21:40:05 +0000

    Delete unneeded scripts, update download-youtube-portion to use superior yt-dlp

--- download-youtube-portion.sh
+++ download-youtube-portion.sh
@@ -11,11 +11,10 @@ else
   read -r video_url
   read -r audio_url
 } < <(
-  youtube-dl --get-url --youtube-skip-dash-manifest "$1"
+  yt-dlp --get-url --youtube-skip-dash-manifest "$1"
 )
 start_time="$2"
 end_time="$3"
 ffmpeg -y -ss "$start_time" -to "$end_time" -i "$video_url" -ss "$start_time" -to "$end_time" -i "$audio_url" \
   -c:v libx264 -crf 15 -movflags +faststart -c:a copy -vsync 0 "$4"
 fi
-
--- grab-youtube-tags.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-youtube-dl "$1" --skip-download --write-info-json | jq '.tags' "$1"
