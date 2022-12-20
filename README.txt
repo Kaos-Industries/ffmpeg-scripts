791d71118679eb5274937c2b9638bb8959c00b2b
Hashim <hashaziz@hotmail.com>
2022-12-20 02:04:32 +0000

    Update concat-with-outro to make extension agnostic

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 TODO:
 
+Test ffmpeg-concat-with-outro.sh still works
 Add comments to crossfade-one-input.sh
 Improve formatting to add spacing and make more readable
 Generate outro in FFmpeg
--- ffmpeg-concat-with-outro.sh
+++ ffmpeg-concat-with-outro.sh
@@ -3,23 +3,15 @@ set -o errexit
 set -o pipefail
 
 if [ $# -lt 2 ]; then
-	echo "USAGE: Pass a video input and the desired output file."
-	echo "Example: script.sh source.mp4"
+	echo "Pass a source file and an output name."
+	echo "Usage: script.sh source.mp4"
 exit
 else
 	for i in "$@"; do
 	height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 "$i" | tr -d $'\r')
-	if [ $height -ge 1080 ]; then
-		echo ""\"$i\"" is at least 1080p: skipping upscaling stage" 
-		echo -e "\nConforming to encoding parameters\n"
-		ffmpeg -y -i "$i" -c:v libx264 -c:a libopus "intermediate.mp4"
-	else
-		echo ""\"$i\"" is too small: upscaling and conforming" 
-		echo -e "\nUpscaling and conforming "\"$i\""\n"
-		ffmpeg -y -i "$i" -vf "scale=-2:1080" -c:v libx264 -c:a libopus "intermediate.mp4"
-	fi
-	echo "file 'intermediate.mp4'" > concat.txt &&
-	echo "file outro.mkv" >> concat.txt
-	ffmpeg -y -f concat -safe 0 -i concat.txt -c copy "${i%.*}_named.mp4"   #  "$2" # "${i%.*}.${i##*.}"
+	ffmpeg -y -i "$i" -vf "scale=-2:'max(1080,ih)':flags=lanczos" -c:v libx264 -c:a libopus "intermediate.${i##*.}"
+	echo "file intermediate.${i##*.}" > concat.txt &&
+	echo "file outro.${i##*.}" >> concat.txt
+	ffmpeg -y -f concat -safe 0 -i concat.txt -c copy "${i%.*}.${i##*.}"
 	done
 fi
