9c234e9558d5aa2b373d0d9c82c014b85e5fcf0c
Hashim <hashaziz@hotmail.com>
2023-01-06 17:47:27 +0000

    Add default channel to allow for running without args

--- count-channel-videos.sh
+++ count-channel-videos.sh
@@ -5,19 +5,23 @@
 set -o errexit
 set -o pipefail
 
+# Set a default channel to track
+mychannel="https://www.youtube.com/channel/UCBLYoY9ZMa4M7z6urJ5fHMg"
+
 usage() {
   echo
-  echo "Pass a link to a YouTube channel to count the amount of videos it's uploaded."
-  echo "usage: `basename $0` https://www.youtube.com/channel/UCBLYoY9ZMa4M7z6urJ5fHMg"
+  echo "Pass a link to a YouTube channel to count the amount of videos it's uploaded, or add a default channel in the script to run without any arguments."
+  echo "usage: `basename $0` [https://www.youtube.com/channel/UCBLYoY9ZMa4M7z6urJ5fHMg]"
   echo " -h --help     Print this help."
   echo " -f --final    Disable the ultrafast preset to produce a final file."
   exit
 }
 
-if [ $# -lt 1 ]; then usage
-elif command -v yt-dlp; then 
-yt-dlp --flat-playlist "$1"
-else
+if command -v yt-dlp 1> /dev/null; then
+if [ $# -ge 1 ]; then 
+yt-dlp --flat-playlist $1
+else yt-dlp --flat-playlist $mychannel
+fi
 echo "This script requires YouTube-DLP: https://github.com/yt-dlp/yt-dlp"
 exit
 fi
